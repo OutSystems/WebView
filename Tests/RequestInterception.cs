c9187852a49970e9767c434d391fe7dac3224dbe
João Neves <joao.neves@outsystems.com>
2017-06-14 19:28:14 +0100

    Added resource handling tests

---
+++ Tests/RequestInterception.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using WebViewControl;
+
+namespace Tests {
+
+    public class RequestInterception : TestBase {
+
+        private const string HtmlWithResource = "<html><script src='resource.js' onerror='scriptFailed = true'></script><body>Test Page</body></html>";
+
+        private static readonly TimeSpan Timeout = TimeSpan.FromSeconds(5);
+
+        protected override bool ReuseWebView {
+            get { return false; }
+        }
+
+        private void LoadAndWaitReady(string html) {
+            var navigated = false;
+            TargetWebView.Navigated += (string url) => navigated = true;
+            TargetWebView.LoadHtml(html);
+            WaitFor(() => navigated, Timeout);
+        }
+
+        private static Stream ToStream(string str) {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(str);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        [Test(Description = "Resource requested is intercepted")]
+        public void ResourceRequestIsIntercepted() {
+            var resourceRequested = "";
+            TargetWebView.BeforeResourceLoad += (WebView.ResourceHandler resourceHandler) => resourceRequested = resourceHandler.Url;
+            LoadAndWaitReady(HtmlWithResource);
+
+            Assert.AreEqual("local://webview/resource.js", resourceRequested);
+        }
+
+        [Test(Description = "Resource response with a stream is loaded properly")]
+        public void InterceptedResourceIsLoaded() {
+            TargetWebView.BeforeResourceLoad += (WebView.ResourceHandler resourceHandler) => resourceHandler.RespondWith(ToStream("scriptLoaded = true"), "js"); // declare x
+            LoadAndWaitReady(HtmlWithResource);
+
+            var loaded = TargetWebView.EvaluateScript<bool>("scriptLoaded"); // check that the value of x is what was declared before in the resource
+            Assert.True(loaded);
+        }
+
+        [Test(Description = "Resource request canceled is not loaded")]
+        public void ResourceRequestIsCanceled() {
+            TargetWebView.BeforeResourceLoad += (WebView.ResourceHandler resourceHandler) => resourceHandler.Cancel();
+            LoadAndWaitReady(HtmlWithResource);
+
+            var failed = TargetWebView.EvaluateScript<bool>("scriptFailed"); // check that the value of x is what was declared before in the resource
+            Assert.True(failed);
+        }
+
+        [Test(Description = "Resource request is redirected")]
+        public void ResourceRedirect() {
+            var redirected = false;
+            const string RedirectUrl = "local://webview/anotherResource.js";
+
+            TargetWebView.BeforeResourceLoad += (WebView.ResourceHandler resourceHandler) => {
+                switch (resourceHandler.Url) {
+                    case "local://webview/resource.js":
+                        resourceHandler.Redirect(RedirectUrl);
+                        break;
+                    case RedirectUrl:
+                        redirected = true;
+                        break;
+                }
+            };
+            LoadAndWaitReady(HtmlWithResource);
+
+            Assert.True(redirected);
+        }
+
+    }
+}
--- Tests/TestBase.cs
+++ Tests/TestBase.cs
@@ -22,33 +22,53 @@ namespace Tests {
             }
 
             window = new Window();
-            webView = new WebView();
-
-            var initialized = false;
-            webView.Navigated += (url) => initialized = true;
-            webView.LoadHtml("<html><script>;</script><body>Test page</body></html>");
-
-            window.Content = webView;
             window.Show();
-
-            // wait for web view to load
-            var start = DateTime.Now;
-            while (!initialized && (DateTime.Now - start).TotalSeconds < 10 && Application.Current != null) {
-                Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(() => Thread.Sleep(1)));
-            }
-
-            if (!initialized) {
-                throw new TimeoutException("Timed out waiting for webview to initialize");
-            }
         }
 
         [SetUp]
         protected void SetUp() {
+            if (webView == null) {
+                webView = new WebView();
+
+                var initialized = false;
+                webView.Navigated += (url) => initialized = true;
+                webView.LoadHtml("<html><script>;</script><body>Test page</body></html>");
+
+                window.Content = webView;
+
+                // wait for web view to load
+                WaitFor(() => initialized, TimeSpan.FromSeconds(10), "webview initialization");
+            }
+
             window.Title = "Running: " + TestContext.CurrentContext.Test.Name;
+        }
+
+        [TearDown]
+        protected void TearDown() {
+            if (!ReuseWebView) {
+                webView.Dispose();
+                window.Content = null;
+                webView = null;
+            }
+        }
+
+        protected virtual bool ReuseWebView {
+            get { return true; }
         }
 
         protected WebView TargetWebView {
             get { return webView; }
+        }
+
+        public static void WaitFor(Func<bool> predicate, TimeSpan timeout, string purpose = "") {
+            var start = DateTime.Now;
+            while (!predicate() && (DateTime.Now - start) < timeout && Application.Current != null) {
+                Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(() => Thread.Sleep(1)));
+            }
+
+            if (!predicate()) {
+                throw new TimeoutException("Timed out waiting for " + purpose);
+            }
         }
     }
 }
