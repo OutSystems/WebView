d4bfd080d8e7532739ce92b823a8964d7ba1c871
João Neves <joao.neves@outsystems.com>
2017-06-14 15:04:30 +0100

    Added unit tests

---
+++ Tests/JavascriptEvaluation.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace Tests {
+
+    public class JavascriptEvaluation : TestBase {
+
+        class TestObject {
+            public string Name;
+            public int Age;
+            public TestObject Parent;
+        }
+
+        [Test(Description = "A simple script evaluates correctly")]
+        public void EvaluateSimpleScript() {
+            var result = TargetWebView.EvaluateScript<int>("2+1");
+            Assert.AreEqual(3, result);
+        }
+
+        [Test(Description = "The order of the executed scripts is respected")]
+        public void ExecutionOrderIsRespected() {
+            try {
+                TargetWebView.ExecuteScript("x = ''");
+                var expectedResult = "";
+                for (var i = 0; i < 10000; i++) {
+                    TargetWebView.ExecuteScript($"x += '{i},'");
+                    expectedResult += i + ",";
+                }
+                var result = TargetWebView.EvaluateScript<string>("x");
+                Assert.AreEqual(expectedResult, result);
+            } finally {
+                TargetWebView.EvaluateScript<bool>("delete x");
+            }
+        }
+
+        [Test(Description = "Evaluation of complex objects returns the expected results")]
+        public void ComplexObjectsEvaluation() {
+            var result = TargetWebView.EvaluateScript<TestObject>(" { Name: 'Snows', Age: 32, Parent: { Name: 'Snows Parent', Age: 60 } }");
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Snows", result.Name);
+            Assert.AreEqual(32, result.Age);
+            Assert.IsNotNull(result.Parent);
+            Assert.AreEqual("Snows Parent", result.Parent.Name);
+            Assert.AreEqual(60, result.Parent.Age);
+        }
+    }
+}
---
+++ Tests/TestBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+using NUnit.Framework;
+using WebViewControl;
+
+namespace Tests {
+
+    [TestFixture]
+    [Apartment(ApartmentState.STA)]
+    public class TestBase {
+
+        private Window window;
+        private WebView webView;
+
+        [OneTimeSetUp]
+        protected void OneTimeSetUp() {
+            if (Application.Current == null) {
+                new Application();
+                Application.Current.ShutdownMode = ShutdownMode.OnLastWindowClose;
+            }
+
+            window = new Window();
+            webView = new WebView();
+
+            var initialized = false;
+            webView.Navigated += (url) => initialized = true;
+            webView.LoadHtml("<html><script>;</script><body>Test page</body></html>");
+
+            window.Content = webView;
+            window.Show();
+
+            // wait for web view to load
+            var start = DateTime.Now;
+            while (!initialized && (DateTime.Now - start).TotalSeconds < 10 && Application.Current != null) {
+                Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(() => Thread.Sleep(1)));
+            }
+
+            if (!initialized) {
+                throw new TimeoutException("Timed out waiting for webview to initialize");
+            }
+        }
+
+        [SetUp]
+        protected void SetUp() {
+            window.Title = "Running: " + TestContext.CurrentContext.Test.Name;
+        }
+
+        protected WebView TargetWebView {
+            get { return webView; }
+        }
+    }
+}
