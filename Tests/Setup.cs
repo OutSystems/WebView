6145706a2601b2632179067f81bffc4c1974be18
João Neves <joao.neves@outsystems.com>
2017-06-20 16:29:17 +0100

    Force kill tests process on tear down

---
+++ Tests/Setup.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Tests {
+
+    [SetUpFixture]
+    class Setup {
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests() {
+            if (TestContext.CurrentContext.Result.FailCount == 0 &&
+                TestContext.CurrentContext.Result.InconclusiveCount == 0 &&
+                TestContext.CurrentContext.Result.SkipCount == 0 &&
+                TestContext.CurrentContext.Result.WarningCount == 0) {
+
+                // kill the process otherwise will hang (due to cef), but only if everything passed
+                ThreadPool.QueueUserWorkItem(_ => {
+                    Thread.Sleep(2000);
+                    Process.GetCurrentProcess().Kill();
+                });
+            }
+        }
+    }
+}
