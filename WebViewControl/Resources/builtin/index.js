be3b788dcd0e0f67485f9fed5fcff1bbf9812491
João Neves <joao.neves@outsystems.com>
2017-11-22 12:08:16 +0000

    Updated CEFSharp Refact tests to introduce support for react web view testing

--- WebViewControl/Resources/builtin/index.js
+++ WebViewControl/Resources/builtin/index.js
@@ -3,16 +3,30 @@ define(["require", "exports", "react", "react-dom"], function (require, exports,
     Object.defineProperty(exports, "__esModule", { value: true });
     function initialize(componentUrl) {
         require([componentUrl], function (WebViewComponentModule) {
-            var WebViewComponent = WebViewComponentModule.default;
+            let WebViewComponent = WebViewComponentModule.default;
             ReactDOM.render(React.createElement(WebViewComponent, null), document.getElementById("webview_root"));
         });
     }
     exports.initialize = initialize;
+    function wrapNativeApi(api) {
+        for (var member in api) {
+            if (api.hasOwnProperty(member) && api[member] instanceof Function) {
+                api[member] = new Proxy(api[member], {
+                    apply: function (target, thisArg, argumentsList) {
+                        if (argumentsList.length > 0) {
+                            var trackCode = argumentsList[0]["TrackCode"];
+                            if (trackCode !== undefined) {
+                                console.log(trackCode);
+                                argumentsList[0] = trackCode;
+                            }
+                        }
+                        return target.apply(thisArg, argumentsList);
+                    }
+                });
+            }
+        }
+    }
+    if (typeof NativeApi !== undefined) {
+        wrapNativeApi(NativeApi);
+    }
 });
-/*function wrapNativeApi(api: Object) {
-    debugger;
-}
-
-if (typeof NativeApi !== undefined) {
-    wrapNativeApi(NativeApi);
-}*/
