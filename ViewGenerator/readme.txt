e0fe47ed0c379e421f9ee817841fa45cadbeae0e
João Neves <joao.neves@outsystems.com>
2018-02-05 19:53:22 +0000

    Added view generator

---
+++ ViewGenerator/readme.txt
@@ -0,0 +1,90 @@
+﻿*****************
+* ViewGenerator *
+*****************
+
+Generates the .Net bindings to the React Web views based on the typescript declarations.
+
+How to use:
+-----------
+
+1) Create a file on the root of your project named ts2lang.json with the following content:
+
+	{
+		"tasks": [
+			{
+				"input": "<directory-of-the-ui-files>",
+				"output": "<directory-to-place-the-generated-files>",
+				"template": "",
+				"parameters": {
+					"namespace": "<namespace-of-your-project>",
+					"baseComponentClass": "<base-component-classname>",
+					"javascriptDistPath": "<path-of-the-generated-js-files>"
+				}
+			}
+		]
+	}
+
+2) You may customize the following parameters in the ts2lang.json file:
+
+	- <directory-of-the-ui-files> 
+		Path where the declaration of the UI (typescript) files live.
+		eg: "View/src/*.tsx"
+
+	- <directory-to-place-the-generated-files> 
+		Path where the UI generated files will be written
+		eg: "Generated/"
+
+	- <namespace-of-your-project> 
+		Namespace to be used in the generated C# files
+		eg: "MyAwesomeProject"
+
+	- <base-component-classname>
+		Name of the base class of the UI component. Use this if you need to inherit from a base component other than the default.
+		eg: "MyCustomBaseControl"
+
+	- <path-of-the-generated-js-files>
+		Place where the typescript compiler will place the compiled js files. Defaults to "dist/"
+
+3) Create the typescript files of your UI. 
+	Eg:
+
+	import * as React from 'react';
+	import "css!../css/styles.css";
+
+	interface IExampleProperties {
+		click(): void;
+	}
+
+	interface IExampleBehaviors {
+		callMe(): void;
+	}
+
+	class Example extends React.Component<IExampleProperties, {}> implements IExampleBehaviors {
+
+		render() {
+			return <Button click={() => this.props.click()}>Click me!</Button>
+		}
+	}
+
+5) Build the project.
+
+6) Include the all the js generated files in the project as Embedded Resources.
+
+7) Build again and run.
+
+
+Notes:
+------
+
+- This tools uses nodejs. If you need to use a different nodejs version you can customize the path of the node executable. 
+Edit you csproj file and search for the following:
+
+  <Target Name="EnsureNuGetPackageBuildImports" BeforeTargets="PrepareForBuild">
+
+Add the following snippet to your csproj file:
+
+  <Target Name="EnsureNuGetPackageBuildImports" BeforeTargets="PrepareForBuild">
+    <PropertyGroup>
+      <NodeJsExe>$(ProjectDir)CustomNodeJs\node.exe</NodeJsExe>
+    </PropertyGroup>
+
---
+++ ViewGenerator/tools/src/ViewGenerator.ts
@@ -0,0 +1,142 @@
+﻿import * as Types from "@outsystems/ts2lang/ts-types";
+import * as Units from "@outsystems/ts2lang/ts-units";
+
+const DelegateSuffix = "Delegate";
+
+function f(input: string) {
+    return (input || "").replace(/\n/g, "\n    ");
+}
+
+function toPascalCase(name: string) {
+    return name[0].toUpperCase() + name.substr(1);
+}
+
+function getFunctionReturnType(func: Units.TsFunction): string {
+    if ((<Types.TsIdentifierType>func.returnType).parameters) {
+        return (<Types.TsIdentifierType>func.returnType).parameters[0].name;
+    }
+    return func.returnType.name;
+}
+
+function getTypeName(tsType: Types.ITsType): string {
+    return tsType.name;
+}
+
+function generateMethodSignature(func: Units.TsFunction, functionSuffix: string = "") {
+    return `${getFunctionReturnType(func)} ${toPascalCase(func.name)}${functionSuffix}(${func.parameters.map(p => getTypeName(p.type) + " " + p.name).join(", ")})`;
+}
+
+function generateProperty(func: Units.TsFunction): string {
+    return (
+        `public delegate ${generateMethodSignature(func, DelegateSuffix)};\n` +
+        `public event ${toPascalCase(func.name)}${DelegateSuffix} ${toPascalCase(func.name)};`
+    );
+}
+
+function generateBehaviorMethod(func: Units.TsFunction): string {
+    return (
+        `public ${generateMethodSignature(func)} {\n` +
+        `    ExecuteMethodOnRoot("${func.name}"${func.parameters.map(p => ", " + p.name).join()});\n` +
+        `}`
+    );
+}
+
+function generateNativeApi(propsInterface: Units.TsInterface | null) {
+    return f(
+        `private class NativeApi : BaseNativeApi<ControlType> {\n` +
+        `    public NativeApi(ControlType owner) : base(owner) { }\n` +
+        `    ${f(propsInterface ? propsInterface.functions.map(f => generateNativeApiMethod(f)).join("\n") : "")}\n` +
+        `}`
+    );
+}
+
+function generateNativeApiMethod(func: Units.TsFunction): string {
+    let isVoid = func.returnType.name === Types.TsVoidType.name;
+    return (
+        `public ${generateMethodSignature(func)} {\n` +
+        `    ${isVoid ? "" : "return "}owner.${toPascalCase(func.name)}?.Invoke(${func.parameters.map(p => p.name).join(", ")});\n` +
+        `}`
+    );
+}
+
+function generateNativeApiObjects(objsInterfaces: Units.TsInterface[]) {
+    return objsInterfaces.map(generateNativeApiObject).join("\n");
+}
+
+function generateNativeApiObject(objInterface: Units.TsInterface) {
+    return f(
+        `public struct ${objInterface.name} {\n` +
+        `    ${objInterface.properties.map(p => `public ${getTypeName(p.type)} ${toPascalCase(p.name)};`).join("\n")}\n` +
+        `}`
+    );
+}
+
+function generateControlBody(propsInterface: Units.TsInterface | null, behaviorsInterface: Units.TsInterface | null) {
+    return f(
+        (propsInterface ? propsInterface.functions.map(f => generateProperty(f)).join("\n") : "") +
+        "\n" +
+        (behaviorsInterface ? behaviorsInterface.functions.map(f => generateBehaviorMethod(f)).join("\n") : "")
+    );
+}
+
+function normalizePath(path: string): string {
+    return path.replace(/\\/g, "/")
+}
+
+export function transform(module: Units.TsModule, context: Object): string {
+    let componentClass = module.classes[0];
+    let propsInterface = module.interfaces.find((ifc) => ifc.name.startsWith("I") && ifc.name.endsWith("Properties")) || null;
+    let behaviorsInterface = module.interfaces.find((ifc) => ifc.name.startsWith("I") && ifc.name.endsWith("Behaviors")) || null;
+    let objects = module.interfaces.filter(ifc => ifc !== propsInterface && ifc !== behaviorsInterface);
+
+    let fullPath = normalizePath(context["$fullpath"] as string);
+    let path = normalizePath(context["$path"] as string);
+
+    let pathDepth = path.split("/").length;
+    let fullPathParts = fullPath.split("/");
+
+    if (pathDepth > 0) {
+        fullPathParts[fullPathParts.length - 2] = context["javascriptDistPath"] || "dist"; // replace
+        // take out the common part of fullpath and path
+        path = fullPathParts.slice(-pathDepth).join("/");
+    }
+
+    // replace file extension with .js
+    let fileExtensionIdx = path.lastIndexOf(".");
+    let fileExtensionLen = path.length - fileExtensionIdx;
+    path = path.substr(0, fileExtensionIdx) + ".js";
+
+    // set the output
+    let filename = fullPathParts[fullPathParts.length - 1].slice(0, -fileExtensionLen);
+
+    let output = normalizePath(context["$output"]);
+    output += (output.charAt(output.length - 1) === "/" ? "" : "/") + filename + ".Generated.cs";
+    context["$output"] = output;
+
+    if (!componentClass) {
+        return "";
+    }
+
+    return (
+        `/*** Auto-generated ***/
+
+namespace ${context["namespace"]} {
+
+    using ControlType = ${componentClass.name};
+
+    public class ${componentClass.name} : ${context["baseComponentClass"] || "WebViewControl.ReactView"} {
+
+        ${f(generateNativeApiObjects(objects))}
+
+        ${f(generateNativeApi(propsInterface))}
+
+        ${f(generateControlBody(propsInterface, behaviorsInterface))} 
+
+        protected override string Source => "${path}";
+
+        protected override object CreateRootPropertiesObject() {
+            return new NativeApi(this);
+        }
+    }
+}`);
+}
