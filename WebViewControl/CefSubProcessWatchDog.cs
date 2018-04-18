dc6ed2d15e4baeb5228d9458021e572556e2cc52
João Neves <joao.neves@outsystems.com>
2018-04-18 14:53:42 +0100

    Speedup process launch

--- WebViewControl/CefSubProcessWatchDog.cs
+++ WebViewControl/CefSubProcessWatchDog.cs
@@ -1,17 +1,20 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace WebViewControl {
 
     internal static class CefSubProcessWatchDog {
 
         public static void StartWatching(string browserSubProcessPath, bool debug) {
-            var cefSubprocessName = Path.GetFileName(browserSubProcessPath);
-            var processStartInfo = new ProcessStartInfo("WebViewWatcher.exe", $"{cefSubprocessName} {(debug ? "1" : "0")}") {
-                CreateNoWindow = true,
-                UseShellExecute = false
-            };
-            Process.Start(processStartInfo);
+            Task.Run(() => {
+                var cefSubprocessName = Path.GetFileName(browserSubProcessPath);
+                var processStartInfo = new ProcessStartInfo("WebViewWatcher.exe", $"{cefSubprocessName} {(debug ? "1" : "0")}") {
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                };
+                Process.Start(processStartInfo);
+            });
         }
     }
 }
