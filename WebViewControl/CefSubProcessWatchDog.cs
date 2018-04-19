d4a3ceb38af0c48c3d532c44866acc9eb93fda4d
João Neves <joao.neves@outsystems.com>
2018-04-19 19:43:52 +0100

    Fixed watch dog cef subprocess name

--- WebViewControl/CefSubProcessWatchDog.cs
+++ WebViewControl/CefSubProcessWatchDog.cs
@@ -11,7 +11,7 @@ namespace WebViewControl {
         public static void StartWatching(string browserSubProcessPath, bool debug) {
             Task.Run(() => {
                 var watcherPath = new Uri(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase), "WebViewWatcher.exe")).LocalPath;
-                var cefSubprocessName = Path.GetFileName(browserSubProcessPath);
+                var cefSubprocessName = Path.GetFileNameWithoutExtension(Path.GetFileName(browserSubProcessPath));
                 var processStartInfo = new ProcessStartInfo(watcherPath, $"{cefSubprocessName} {(debug ? "1" : "0")}") {
                     CreateNoWindow = true,
                     UseShellExecute = false
