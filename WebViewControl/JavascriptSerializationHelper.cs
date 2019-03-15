9c390ad3c862afdb5fc20f4aa206a512fc3a14a6
João Neves <joao.neves@outsystems.com>
2019-03-15 21:55:31 +0000

    Improved js serialization

--- WebViewControl/JavascriptSerializationHelper.cs
+++ WebViewControl/JavascriptSerializationHelper.cs
@@ -3,22 +3,24 @@ using System.Collections;
 using System.Linq;
 using System.Web;
 using JavascriptObject = System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, object>>;
+using SerializationHandler = System.Func<object, string>;
 
 namespace WebViewControl {
 
     public static class JavascriptSerializer {
 
-        public static string Serialize(object o) {
+        public static string Serialize(object o, SerializationHandler handleComplexType = null) {
             if (o == null) return "null";
             if (o is string str) return Serialize(str);
             if (o is IEnumerable col) return Serialize(col);
             if (o is ValueType) return o.ToString().ToLowerInvariant();
             if (o is JavascriptObject jso) return Serialize(jso);
+            if (handleComplexType != null) return handleComplexType(o);
             return o.ToString();
         }
 
-        public static string Serialize(JavascriptObject o) {
-            return "{" + string.Join(",", o.Select(kvp => Serialize(kvp.Key) + ":" + Serialize(kvp.Value))) + "}";
+        public static string Serialize(JavascriptObject o, SerializationHandler handleComplexType = null) {
+            return "{" + string.Join(",", o.Select(kvp => Serialize(kvp.Key) + ":" + Serialize(kvp.Value, handleComplexType))) + "}";
         }
 
         public static string Serialize(string str) {
@@ -29,8 +31,8 @@ namespace WebViewControl {
             return boolean.ToString().ToLowerInvariant();
         }
 
-        public static string Serialize(IEnumerable arr) {
-            return "[" + string.Join(",", arr.Cast<object>().Select(i => Serialize(i))) + "]";
+        public static string Serialize(IEnumerable arr, SerializationHandler handleComplexType = null) {
+            return "[" + string.Join(",", arr.Cast<object>().Select(i => Serialize(i, handleComplexType))) + "]";
         }
     }
 }
