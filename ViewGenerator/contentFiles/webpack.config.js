538384c959865ee01bd130595b4e0574bb7ab851
Miguel Alves <alvesmiguel1@users.noreply.github.com>
2019-06-24 14:15:34 +0100

    #RICT-1654 Added webpack in ViewGenerator

---
+++ ViewGenerator/contentFiles/webpack.config.js
@@ -0,0 +1,23 @@
+﻿const fs = require('fs');
+const entryMap = {};
+
+fs.readdirSync('.')
+    .filter(file => file.match(/.*\.view.tsx$/))
+    .forEach(f => { entryMap[f.replace(/\.view.tsx$/, '')] = ['./' + f]; });
+
+module.exports = {
+    mode: "development",
+    entry: entryMap,
+    output: {
+        filename: "dummybundle.js"
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            { test: /\.css?$/, loader: "style-loader!css-loader" },
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    }
+};
