0322e4dd13463889cb9f918ec4b985122d1ed9e7
Miguel Alves <alvesmiguel1@users.noreply.github.com>
2019-06-25 16:37:43 +0100

    #RICT-1654 Added webpack in ViewGenerator

---
+++ ViewGeneratorCore/tools/webpack.config.js
@@ -0,0 +1,44 @@
+﻿const glob = require('glob');
+const path = require('path');
+
+const bindingsMap = {};
+const entryMap = {};
+
+/** 
+ *  Get input and output entries from ts2lang file
+ * */
+let bindings = require('./ts2lang.json');
+bindings.tasks.forEach(t => bindingsMap[t.input] = t.output);
+
+/** 
+ *  Build an entry map for webpack config
+ * */
+Object.keys(bindingsMap).forEach(input => {
+    let output = bindingsMap[input];
+    glob.sync(input).forEach(f => {
+
+        let filePath = path.parse(f);
+
+        // Exclude node_modules files
+        if (!f.includes("node_modules")) {
+            entryMap[output + filePath.name] = ['./' + f];
+        }
+    });
+});
+
+module.exports = {
+    entry: entryMap,
+    output: {
+        path: __dirname,
+        filename: "[name].js"
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            { test: /\.css?$/, loader: "style-loader!css-loader" },
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    }
+};
