9ef090d77cb18e57478bb5496da3310a3075d9d6
Miguel Alves <alvesmiguel1@users.noreply.github.com>
2019-06-27 14:56:44 +0100

    #RICT-1654 Bundle React, React-DOM, Prop-Types, and ViewFrame separately

---
+++ ReactViewResources/webpack.config.js
@@ -0,0 +1,40 @@
+﻿const path = require('path');
+
+module.exports = {
+
+    target: 'node',
+
+    entry: {
+        'react-view-resources': [
+            './node_modules/react/umd/react.production.min.js',
+            './node_modules/react-dom/umd/react-dom.production.min.js',
+            './node_modules/prop-types/prop-types.min.js',
+            './libs/ViewFrame.tsx'
+        ]
+    },
+
+    output: {
+        path: path.resolve('.'),
+        filename: "ReactViewResources.js"
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            configFile: "libs/tsconfig.json"
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
