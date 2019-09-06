23402b9e5368580939c4ffb6f78ce0abfd0cbdeb
João Neves <joao.neves@outsystems.com>
2019-09-06 17:47:50 +0100

    Logs

--- Example/App.xaml.cs
+++ Example/App.xaml.cs
@@ -5,11 +5,17 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WebViewControl;
 
 namespace Example {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+
+        protected override void OnStartup(StartupEventArgs e) {
+            WebView.LogFile = "ceflog.txt";
+            base.OnStartup(e);
+        }
     }
 }
