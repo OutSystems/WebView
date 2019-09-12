208ab57d2a81023c2ab17e17df31b5be846a52cd
João Neves <joao.neves@outsystems.com>
2019-09-12 16:58:10 +0100

    Linked builtin modules with their definitions

--- ReactViewResources/Loader/ResourceLoader.ts
+++ ReactViewResources/Loader/ResourceLoader.ts
@@ -1,6 +1,5 @@
 ﻿import * as React from "react";
-
-type ResourceLoaderUrlFormatter = (resourceKey: string) => string;
+import { ResourceLoaderUrlFormatter } from "./../../ViewGenerator/contentFiles/node_modules/@types/ResourceLoader";
 
 export const ResourceLoader = React.createContext<ResourceLoaderUrlFormatter>(() => "");
 
