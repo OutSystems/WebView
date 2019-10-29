6b83c7ba3f920e71306ead58574fff3aab7d20a6
João Neves <joao.neves@outsystems.com>
2019-10-29 11:53:42 +0000

    Fixed custom resources load

--- ReactViewControl/ReactViewRender.cs
+++ ReactViewControl/ReactViewRender.cs
@@ -530,7 +530,7 @@ namespace ReactViewControl {
                             var response = customResourceRequestedHandlers.Select(h => h(resourceKey, options)).FirstOrDefault(r => r != null);
 
                             if (response != null) {
-                                var path = uri.AbsolutePath;
+                                var path = uri.AbsoluteUri;
                                 var extension = Path.GetExtension(path).TrimStart('.');
                                 resourceHandler.RespondWith(response, extension);
                             } else {
