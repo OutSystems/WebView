f8d8d62ba528339ebd0220691af4b247e4f421b2
Tiago Braga <braga.tiago.93@gmail.com>
2020-03-09 17:23:32 +0000

    Remove unused usings

--- WebViewControl/WebView.InternalDragHandler.cs
+++ WebViewControl/WebView.InternalDragHandler.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xilium.CefGlue;
+﻿using Xilium.CefGlue;
 using Xilium.CefGlue.Common.Handlers;
 
 namespace WebViewControl {
