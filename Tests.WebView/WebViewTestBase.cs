3895693415ef4a445efec552fd4d3747e55757a3
alvesmiguel1 <miguel.alves@outsystems.com>
2020-08-04 18:36:54 +0100

    Code review

--- Tests.WebView/WebViewTestBase.cs
+++ Tests.WebView/WebViewTestBase.cs
@@ -30,19 +30,16 @@ namespace Tests.WebView {
             
             void OnNavigated(string url, string frameName) {
                 if (url != UrlHelper.AboutBlankUrl) {
-                    taskCompletionSource.SetResult(true);
-                    TargetView.Navigated -= OnNavigated;
+                    try {
+                        taskCompletionSource.SetResult(true);
+                    } finally {
+                        TargetView.Navigated -= OnNavigated;
+                    }
                 }
             }
-
             TargetView.Navigated += OnNavigated;
-            try {  
-                TargetView.LoadHtml(html);
-                return taskCompletionSource.Task;
-
-            } finally {
-                TargetView.Navigated -= OnNavigated;
-            }
+            TargetView.LoadHtml(html);
+            return taskCompletionSource.Task;
         }
 
         protected void WithUnhandledExceptionHandling(Action action, Func<Exception, bool> onException) {
