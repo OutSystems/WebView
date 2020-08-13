42e25d218a8e11baca49a91f89aa587e6da44c3e
heldergoncalves92 <heldergoncalves92@gmail.com>
2020-08-13 11:03:39 +0100

    Refresh StyleSheet Test

--- Tests.ReactView/DefaultStylesheetLoadTests.cs
+++ Tests.ReactView/DefaultStylesheetLoadTests.cs
@@ -7,16 +7,26 @@ namespace Tests.ReactView {
 
     public class DefaultStyleSheetLoadTests : ReactViewTestBase {
 
+        private static ReactViewWithStyleSheet currentView;
+        private static bool loadPrimaryStyleSheet = true;
+
         protected class ViewFactoryWithStyleSheet : TestReactViewFactory {
-            public override ResourceUrl DefaultStyleSheet => new ResourceUrl(typeof(DefaultStyleSheetLoadTests).Assembly, "Generated", "Default.css");
+            public override ResourceUrl DefaultStyleSheet => loadPrimaryStyleSheet ?
+                new ResourceUrl(typeof(DefaultStyleSheetLoadTests).Assembly, "Generated", "Default.css") :
+                new ResourceUrl(typeof(DefaultStyleSheetLoadTests).Assembly, "Generated", "Default_2.css");
         }
 
         protected class ReactViewWithStyleSheet : TestReactView {
             protected override ReactViewFactory Factory => new ViewFactoryWithStyleSheet();
+
+            public void RefreshStyleSheetTests() {
+                RefreshDefaultStyleSheet();
+            }
         }
 
         protected override TestReactView CreateView() {
-            return new ReactViewWithStyleSheet();
+            currentView = new ReactViewWithStyleSheet();
+            return currentView;
         }
 
         [Test(Description = "Tests default stylesheets get loaded")]
@@ -33,6 +43,33 @@ namespace Tests.ReactView {
 
                 StringAssert.Contains(".bar", stylesheet);
             });
+        }
+
+        [Test(Description = "Tests default stylesheets get loaded when refreshed")]
+        public async Task SecondaryStylesheetIsLoadedWhenDefaultStyleSheetIsRefreshed() {
+            // Arrange
+            var initialLoadPrimaryStyleSheet = loadPrimaryStyleSheet;
+            loadPrimaryStyleSheet = false;
+            
+            // Act
+            currentView.RefreshStyleSheetTests();
+
+            // Asssert
+            await Run(async () => {
+                var taskCompletionSource = new TaskCompletionSource<string>();
+
+                TargetView.Event += (args) => {
+                    taskCompletionSource.SetResult(args);
+                };
+
+                TargetView.ExecuteMethod("checkStyleSheetLoaded", "2");
+                var stylesheet = await taskCompletionSource.Task;
+
+                StringAssert.Contains(".bazz-version-2", stylesheet);
+            });
+
+            // Tear down
+            loadPrimaryStyleSheet = initialLoadPrimaryStyleSheet;
         }
     }
 }
