0c9d429fba1264b5d02e162a0b7d73565c78b706
João Malés <joao.males@outsystems.com>
2021-01-19 14:42:28 +0000

    Upgraded CefGlue version.

--- SampleWebView.Avalonia/bundle.sh
+++ SampleWebView.Avalonia/bundle.sh
@@ -3,5 +3,5 @@
 dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=osx-x64
 
 TARGETAPP=bin/Debug/net5.0/osx-x64/publish/SampleWebView.app/Contents/MacOS
-chmod +x "$TARGETAPP/Xilium.CefGlue.BrowserProcess"
+chmod +x "$TARGETAPP/CefGlueBrowserProcess/Xilium.CefGlue.BrowserProcess"
 chmod +x "$TARGETAPP/SampleWebView.Avalonia"
