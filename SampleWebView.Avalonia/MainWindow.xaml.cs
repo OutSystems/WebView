609aabd580daa664f31580e3c33558df8c03c526
Hélder Gonçalves <helder.goncalves@outsystems.com>
2022-12-13 18:13:08 +0000

    Reproduce issue

--- SampleWebView.Avalonia/MainWindow.xaml.cs
+++ SampleWebView.Avalonia/MainWindow.xaml.cs
@@ -1,17 +1,31 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using WebViewControl;
 
 namespace SampleWebView.Avalonia {
 
     internal class MainWindow : Window {
+        private static bool flag = true;
 
         public MainWindow() {
-            WebView.Settings.OsrEnabled = false;
-            WebView.Settings.LogFile = "ceflog.txt";
+            if (flag) {
+                flag = false;
+                WebView.Settings.OsrEnabled = false;
+                WebView.Settings.LogFile = "ceflog.txt";
+            }
+            
             AvaloniaXamlLoader.Load(this);
-
             DataContext = new MainWindowViewModel(this.FindControl<WebView>("webview"));
+
+            var btn = this.FindControl<Button>("AmazingButton");
+            btn.Click += BtnOnClick;
+        }
+
+        private void BtnOnClick(object sender, RoutedEventArgs e) {
+            var newWindow = new MainWindow();
+            newWindow.Show(this);
         }
     }
 }
