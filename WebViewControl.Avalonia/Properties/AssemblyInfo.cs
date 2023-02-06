43ea809b81f11f4e7439e47b6404a21f530655a1
Luiz Santos <luiz.santos@outsystems.com>
2023-02-06 15:23:04 +0000

    Remove WPF

--- WebViewControl.Avalonia/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Tests.ReactView")]
-[assembly: InternalsVisibleTo("Tests.WebView")]
-[assembly: InternalsVisibleTo("ReactViewControl.Avalonia")]
