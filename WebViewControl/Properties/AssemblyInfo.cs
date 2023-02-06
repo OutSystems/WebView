303648e91dde8b3c496249aafd3a65c2e6f26fbc
Luiz Santos <luiz.santos@outsystems.com>
2023-02-06 19:46:19 +0000

    Cleanup AssemblyInfo

--- WebViewControl/Properties/AssemblyInfo.cs
+++ WebViewControl/Properties/AssemblyInfo.cs
@@ -1,13 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1c2a0c7-df81-4a8f-aeb5-b5375d5d1b47")]
 
 [assembly: InternalsVisibleTo("Tests.ReactView")]
 [assembly: InternalsVisibleTo("Tests.WebView")]
