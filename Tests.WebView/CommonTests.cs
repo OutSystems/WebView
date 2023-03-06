312df60d1c845a9514eae5aa1bc602db67c868db
José Santos <jfnfsantos@gmail.com>
2023-03-06 10:16:29 +0000

    Bumping cef and optimizing local packages (#297)
    * Refactor EvaluateScript to include the 'return' keyword
    
    * Bump versions
    
    * bumping cef and optimizing local packages
    
    * Fix identation and bump webview version
    
    * revert central package pinning
    
    * Isolating and Removing test
    
    ---------
    
    Co-authored-by: brunocunhasilva <bruno.cunhasilva@outsystems.com>

--- Tests.WebView/CommonTests.cs
+++ Tests.WebView/CommonTests.cs
@@ -57,34 +57,5 @@ namespace Tests.WebView {
                 }
             });
         }
-
-        [Test(Description = "Tests that the webview is disposed when host window is not shown")]
-        public async Task WebViewIsNotDisposedWhenUnloaded() {
-            await Run(async () => {
-                var taskCompletionSource = new TaskCompletionSource<bool>();
-                var view = new WebViewControl.WebView();
-                view.Disposed += delegate {
-                    taskCompletionSource.SetResult(true);
-                };
-
-                var window = new Window {
-                    Title = CurrentTestName,
-                    Content = view
-                };
-
-                try {
-                    window.Show();
-                    window.Content = null;
-                    Assert.IsFalse(taskCompletionSource.Task.IsCompleted);
-
-                    window.Content = view;
-                    window.Close();
-                    var disposed = await taskCompletionSource.Task;
-                    Assert.IsTrue(disposed);
-                } finally {
-                    window.Close();
-                }
-            });
-        }
     }
 }
