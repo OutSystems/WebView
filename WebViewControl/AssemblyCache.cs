400c0f5a902d63d8347c882ff2fbd1586079537a
Hugo Lourenço <hlouren@users.noreply.github.com>
2024-02-16 08:02:46 +0000

    Update WebViewControl/AssemblyCache.cs
    Co-authored-by: Miguel Alves <alvesmiguel1@users.noreply.github.com>

--- WebViewControl/AssemblyCache.cs
+++ WebViewControl/AssemblyCache.cs
@@ -16,7 +16,7 @@ namespace WebViewControl {
         // Note that no guarantee is provided about which version is resolved if there are multiple loaded assemblies
         // with the same name and no specific version is provided.
         // This, consumer apps are encouraged to include the version in the resource url
-        private Dictionary<(string AssemblyName, Version AssemblyVersion), Assembly> assemblies;
+        private IDictionary<(string AssemblyName, Version AssemblyVersion), Assembly> assemblies;
         
         private bool newAssembliesLoaded = true;
 
