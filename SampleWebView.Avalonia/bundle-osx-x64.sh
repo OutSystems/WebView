7da38be3451fc77b7d35230435224031ff26bc66
Tiago Braga <tbragaf@users.noreply.github.com>
2024-05-15 17:26:58 +0100

    Add support for .NET 8 (#337)
    * Add support for net 8
    
    * Fix sample apps target framework
    
    * Try multiple targets
    
    * Raise version
    
    * Add suffix to target frameworks
    
    * revert sufix changes
    
    * Tentative of adding suffixes
    
    * Tentative of adding suffixes
    
    * Review code
    
    * Fix windows version
    
    * revert
    
    * Fix
    
    * tentative fix tests

--- SampleWebView.Avalonia/bundle-osx-x64.sh
+++ SampleWebView.Avalonia/bundle-osx-x64.sh
@@ -2,6 +2,6 @@
 
 dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:Platform=x64
 
-TARGETAPP=bin/x64/Debug/net6.0/osx-x64/publish/SampleWebView.app/Contents/MacOS
+TARGETAPP=bin/x64/Debug/net8.0/osx-x64/publish/SampleWebView.app/Contents/MacOS
 chmod +x "$TARGETAPP/CefGlueBrowserProcess/Xilium.CefGlue.BrowserProcess"
 chmod +x "$TARGETAPP/SampleWebView.Avalonia"
